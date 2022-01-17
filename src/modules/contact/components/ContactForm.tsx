7aa109e2741378b324171f62db60295e9a6241ba
nlam-grok <nathan.lam@grokacademy.org>
2022-01-17 19:38:15 +1100

    Add contact form

---
+++ src/modules/contact/components/ContactForm.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ContactForm = () => {
+  return <>
+    <div>
+      <label>Name</label>
+      <input type={'text'}></input>
+    </div>
+    <div>
+      <label>Content</label>
+      <input type={'textarea'}></input>
+    </div>
+  </>
+};
+
+export default ContactForm;
